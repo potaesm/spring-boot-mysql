d9b23f53846c16cabaefa3773c5da5229fe31fa0
Suthinan Musitmani <suthinan.musitmani@allianz.com>
2020-09-18 17:07:49 +0700

    Add proxy for restTemplate

--- src/main/java/com/suthinan/mysql/config/rest/JsonPlaceHolderRestTemplate.java
+++ src/main/java/com/suthinan/mysql/config/rest/JsonPlaceHolderRestTemplate.java
@@ -8,19 +8,31 @@ import com.suthinan.mysql.model.ToDoModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.net.Proxy.Type;
 
 @Configuration
 public class JsonPlaceHolderRestTemplate {
     @Bean
     public ResponseEntity<?> getToDosFromJsonPlaceHolder() throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
+        // Allianz Proxy
+        Proxy proxy = new Proxy(Type.HTTP, new InetSocketAddress("10.186.208.15", 8080));
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setProxy(proxy);
+        // Rest Template With Proxy
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        // Rest Template Without Proxy
+        // RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity("http://jsonplaceholder.typicode.com/todos", String.class);
+        // Mapper
         ObjectMapper mapper = new ObjectMapper();
         List<JsonPlaceHolderModel> todosJsonPlaceHolderList = Arrays.asList(mapper.readValue(response.getBody(), JsonPlaceHolderModel[].class));
         List<ToDoDto> toDoDtos = new ArrayList<>();
